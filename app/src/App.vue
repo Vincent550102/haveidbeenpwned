c86c0ed1651bc082fa77e3b36bf2d87d4e4163fb
時流 <jiru@users.noreply.github.com>
2023-07-01 15:50:07 +0800

    Translate all strings

--- app/src/App.vue
+++ app/src/App.vue
@@ -73,21 +73,21 @@ export default {
           class="peer text-white h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-green-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
         <label
           class="after:content[' '] pointer-events-none absolute left-0 -top-2.5 flex h-full w-full select-none text-sm font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-green-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-green-500 peer-focus:after:scale-x-100 peer-focus:after:border-green-500 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-          ID number
+          {{ t("message.ID") }}
         </label>
       </div>
       <div class="">
         <button @click="checkHash()" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-          Check
+          {{ t("message.check") }}
         </button>
       </div>
       <div>
-        <h2 class="text-xl text-white font-bold">Leaked in datasets:</h2>
+        <h2 class="text-xl text-white font-bold">{{ t("message.leaked_title") }}</h2>
         <div v-for="leak in leaks" v-key="leak.name">
           <h2 class="text-xltext-white  font-bold">{{ leak.name }}</h2>
           <ul class="flex flex-wrap items-center justify-center mb-6 text-gray-900">
             <li v-for="column in leak.columns" v-key="column" class="mr-4 text-white hover:underline md:mr-6">
-              {{ column }}
+              {{ t("column." + column, column) }}
             </li>
           </ul>
         </div>
